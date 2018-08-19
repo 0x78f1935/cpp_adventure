ea1bc4d1b547ee6ff9e230069d0263b9ce401d2f
annihilator708 <annihilator708@hotmail.com>
2018-08-19 03:04:31 +0200

    cpp examples

---
+++ ConstructorParametersAndOverloading/src/Person.cpp
@@ -0,0 +1,24 @@
+#include "Person.h"
+#include <sstream>
+
+Person::Person() {
+	name = "undefined";
+	age = 0;
+}
+
+Person::Person(string newName, int newAge) {
+	name = newName;
+	age = newAge;
+}
+
+string Person::toString() {
+	stringstream ss;
+
+	ss << "Name: ";
+	ss << name;
+	ss << "; age: ";
+	ss << age;
+
+	return ss.str();
+
+}
