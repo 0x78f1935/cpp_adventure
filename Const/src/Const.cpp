41ad8f20fd0d2295daf23345f538ee2073cff371
annihilator708 <annihilator708@hotmail.com>
2018-08-19 18:37:05 +0200

    cpp examples

---
+++ Const/src/Const.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+private:
+	string name;
+public:
+	void setName(string name) { this->name = name; };
+	void speak() const { cout << "My name is: " << name << endl; };
+};
+
+int main() {
+
+	const double PI = 3.141592;
+	cout << PI << endl;
+
+	Animal animal;
+	animal.setName("Freddy");
+	animal.speak();
+
+	int value = 8;
+	int *pValue = &value;
+	cout << *pValue << endl;
+
+	int number = 11;
+	pValue = &number; // This will result in a error. | int *const pValue = &value;
+	*pValue = 12; // This will result in a error. | const int *pValue = &value;
+	// This will result in both breaking. | const int * const pValue = &value;
+
+	cout << *pValue << endl;
+
+	return 0;
+}
