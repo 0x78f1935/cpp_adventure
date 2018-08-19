147c37266fdfbb85be9e71dde7a183152c2390ce
annihilator708 <annihilator708@hotmail.com>
2018-08-19 03:48:30 +0200

    cpp examples

---
+++ this2/src/this.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "person.h"
+using namespace std;
+
+int main() {
+	Person person1;
+	Person person2("Bob", 42);
+	Person person3("Sue", 23);
+
+	cout << person1.toString() << endl;
+	cout << person2.toString() << endl;
+	cout << person3.toString() << endl;
+
+	return 0;
+}
