58404ec43f1b3688d4cac7b9f1f686e0cdd484dd
annihilator708 <annihilator708@hotmail.com>
2018-08-19 04:36:42 +0200

    cpp examples

--- Arithmetic/src/Arithmetic.cpp
+++ Arithmetic/src/Arithmetic.cpp
@@ -31,11 +31,14 @@ int main() {
 	value4 /= 5; // value4 = value4 / 5;
 	cout << value4 << endl;
 
-	int value5 = 12%5;
+	int value5 = 12 % 5;
 	cout << value5 << endl; // value5 = value5 / 5 take left overs
 
-	double equation = ((5/4)%2)+(2.3*6); // 5/4=2; 2%2=0; 2.3*6=14.8;
+	double equation = ((5 / 4) % 2) + (2.3 * 6); // 5/4=2; 2%2=0; 2.3*6=14.8;
 	cout << equation << endl;
+
+	int time = 920;
+	clock(time);
 
 	return 0;
 }
