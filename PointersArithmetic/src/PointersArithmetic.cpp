47cde705ed8fbcec023fd95240d05045c7e3d8df
annihilator708 <annihilator708@hotmail.com>
2018-08-19 16:54:06 +0200

    cpp examples

---
+++ PointersArithmetic/src/PointersArithmetic.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	const int NSTRINGS = 5;
+	string texts[NSTRINGS] = {"one", "two", "three", "four", "five"};
+
+	string *pTexts = texts;
+
+	pTexts += 3;
+
+	cout << *pTexts << endl;
+
+	pTexts -= 2;
+
+	cout << *pTexts << endl;
+
+	string *pEnd = &texts[NSTRINGS];
+	pTexts = &texts[0];
+
+	while (pTexts != pEnd) {
+		cout << *pTexts << endl;
+		pTexts++;
+	}
+
+	// Set pTexts back to start.
+	pTexts = &texts[0];
+	long elements = (long) (pEnd - pTexts); // get numbers of elements in a array
+
+	cout << elements << endl;
+
+	pTexts += NSTRINGS/2;
+	cout << *pTexts << endl;
+
+	return 0;
+}
